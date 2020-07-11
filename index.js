1c78bd1590b8129ae7e1814d9ef8a17525da7d8c
ashutoshkumar00 <67230750+ashutoshkumar00@users.noreply.github.com>
2020-07-11 12:47:53 +0530

    add to website

---
+++ index.js
@@ -0,0 +1,52 @@
+var randomnumber1=Math.floor(Math.random()*6)+1;
+if(randomnumber1===1){
+document.querySelector(".img1").setAttribute("src","dice1.png");}
+
+if(randomnumber1===2){
+document.querySelector(".img1").setAttribute("src","dice2.png");}
+
+if(randomnumber1===3){
+document.querySelector(".img1").setAttribute("src","dice3.png");}
+
+if(randomnumber1===4){
+document.querySelector(".img1").setAttribute("src","dice4.png");}
+
+if(randomnumber1===5){
+document.querySelector(".img1").setAttribute("src","dice5.png");}
+
+if(randomnumber1===6){
+document.querySelector(".img1").setAttribute("src","dice6.png");}
+
+var randomnumber2=Math.floor(Math.random()*6)+1;
+
+if(randomnumber2===1){
+document.querySelector(".img2").setAttribute("src","dice1.png");}
+
+
+if(randomnumber2===2){
+document.querySelector(".img2").setAttribute("src","dice2.png");}
+
+
+if(randomnumber2===3){
+document.querySelector(".img2").setAttribute("src","dice3.png");}
+
+
+if(randomnumber2===4){
+document.querySelector(".img2").setAttribute("src","dice4.png");}
+
+
+if(randomnumber2===5){
+document.querySelector(".img2").setAttribute("src","dice5.png");}
+
+
+if(randomnumber2===6){
+document.querySelector(".img2").setAttribute("src","dice6.png");}
+
+if(randomnumber1>randomnumber2)
+{  document.querySelector("h1").innerHTML="🚩Player 1 Wins ";}
+
+if(randomnumber1<randomnumber2)
+{
+    document.querySelector("h1").innerHTML=" Player 2 Wins 🚩";}
+
+if(randomnumber1===randomnumber2){document.querySelector("h1").innerHTML="Draw";}
